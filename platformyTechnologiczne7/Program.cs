14b962ccd27815ad6d5a234f6bf73e798671ccea
zuzqqa <76653705+zuzqqa@users.noreply.github.com>
2024-04-22 17:43:45 +0200

    Add project files.

---
+++ platformyTechnologiczne7/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+AppContext.SetSwitch("System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization", true);
+
+
+Console.WriteLine("Hello, World!");
+
+var formatter = new BinaryFormatter();
