6ce9fdde8bd82c33d0501590c43c47b89b92e6a7
zuzqqa <76653705+zuzqqa@users.noreply.github.com>
2024-04-22 18:25:03 +0200

    first 2 excercises

--- platformyTechnologiczne7/Program.cs
+++ platformyTechnologiczne7/Program.cs
@@ -1,11 +1,77 @@
-﻿using System.Runtime.Serialization.Formatters.Binary;
+﻿
+using System.Runtime.Serialization.Formatters.Binary;
 
-AppContext.SetSwitch("System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization", true);
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Nie podano sciezki katalogu.");
+        }
 
-// command line arguments
+        string directoryPath = args[0];
 
-var arguments = Environment.GetCommandLineArgs();
+        Console.WriteLine("=====ZADANIE 1=====\n");
+        ContentsOfTheDirectory(directoryPath);
 
-Console.WriteLine("GetCommandLineArgs: {0}", string.Join(", ", arguments));
+        Console.WriteLine("=====ZADANIE 2=====\n");
+        ContentsOfTheDirectoryRecursively(directoryPath);
+
+        AppContext.SetSwitch("System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization", true);
+    }
+
+    static void ContentsOfTheDirectory(string directoryPath)
+    {
+        if (!Directory.Exists(directoryPath))
+        {
+            Console.WriteLine("Podany katalog nie istnieje.");
+        }
+
+        string[] files = Directory.GetFiles(directoryPath);
+        string[] directories = Directory.GetDirectories(directoryPath);
+
+        Console.WriteLine("Pliki w katalogu: ");
+        foreach (string file in files) { Console.WriteLine("    " + Path.GetFileName(file)); }
+
+        Console.WriteLine("\nPodkatalogi: ");
+        foreach (string d in directories) { Console.WriteLine("    " + Path.GetFileName(d)); }
+
+        Console.WriteLine("\n");
+    }
+
+    static void ContentsOfTheDirectoryRecursively(string directoryPath, string indent = " ")
+    {
+        if (!Directory.Exists(directoryPath))
+        {
+            Console.WriteLine("Podany katalog nie istnieje.");
+        }
+
+        string[] files = Directory.GetFiles(directoryPath);
+        string[] directories = Directory.GetDirectories(directoryPath);
+
+        if (files.Length > 0)
+        {
+            Console.WriteLine($"{indent}Pliki w katalogu: ");
+            foreach (var file in files)
+            {
+                Console.WriteLine($"{indent}- {Path.GetFileName(file)}");
+            }
+        }
+
+        if (directories.Length > 0)
+        {
+            Console.WriteLine($"\n{indent}Podkatalogi: ");
+
+            foreach (var d in directories)
+            {
+                Console.WriteLine($"{indent}- {Path.GetFileName(d)}");
+                ContentsOfTheDirectoryRecursively(d, indent + "   "); // Wywołanie rekurencyjne z większym wcięciem
+            }
+        }
+        
+    }
 
 /*var formatter = new BinaryFormatter();*/
+}
+
